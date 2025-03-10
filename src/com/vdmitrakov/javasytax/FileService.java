ab065ee74b52bf7a99d935d0b605984201908dd2
Volodimir Dmitrakov <vdmit19@gmail.com>
2025-03-10 10:52:25 +0200

    Class and methods modifiers have been changed 10.03.2025

--- src/com/vdmitrakov/javasytax/FileService.java
+++ src/com/vdmitrakov/javasytax/FileService.java
@@ -7,11 +7,11 @@ import java.nio.file.StandardOpenOption;
 
 
  class FileService {
-    public static String readFile(String filePath) throws IOException {
+    static String readFile(String filePath) throws IOException {
         return Files.readString(Path.of(filePath));
     }
 
-    public static void writeFile(String filePath, String content) throws IOException {
+     static void writeFile(String filePath, String content) throws IOException {
         Files.writeString(Path.of(filePath), content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
